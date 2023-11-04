c91cf98f8f08da18f369c558bab3c1f1bf8fd4ae
diegoballesteros <diegoballesteros777@gmail.com>
2023-11-04 13:38:56 -0500

    agregando entidad barberShop

--- src/main/java/com/gobarber/demo/persistence/entity/BarberShopEntity.java
+++ src/main/java/com/gobarber/demo/persistence/entity/BarberShopEntity.java
@@ -1,31 +1,33 @@
 package com.gobarber.demo.persistence.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "barbers")
-@Setter
+@Table("Barbershops")
 @Getter
+@Setter
 @NoArgsConstructor
-public class BarberEntity {
+public class BarberShopEntity {
+
     @Id
-    @Column(name = "id_barber", nullable = false, length = 50)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idBarber;
+    @Column(name = "id_barbershop", nullable = false)
+    private Integer idBarberShop;
 
     @Column(nullable = false, length = 100)
     private String name;
 
-    @Column(name = "phone_number", nullable = false, length = 30)
+    @Column(name = "phone_number", length = 30)
     private String phoneNumber;
 
     @Column(length = 100)
     private String email;
 
-    @Column(name = "image_logo")
-    private String logo;
+    private String image;
+    
 
 }
